9af63c255601ea69a04ee864179808d66815057c
prartt <116332833+prartt@users.noreply.github.com>
2024-03-26 22:44:18 +0700

    🚀 Model half part
    -Model first part
    -class User not included yet
    -some foreign key have not connected

--- src/services/models/__init__.py
+++ src/services/models/__init__.py
@@ -1,1 +1,8 @@
 from .models import *
+from .chat import Chat
+from .message import Message
+from .owns import Owns
+# from .is_friend_of import IsFriendOf
+from .achievement import Achievement
+from .experience import Experience
+from .physical_attribute import PhysicalAttribute
