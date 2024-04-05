eb5c1c20d41430091a671c3759af60fc7d4ec3a6
Pataradanai Hansuksakul <116214675+Pataradanaimcky@users.noreply.github.com>
2024-04-06 01:32:33 +0700

    Feat: create mock event

--- src/services/utils/mock_event.py
+++ src/services/utils/mock_event.py
@@ -8,15 +8,10 @@ fake = Faker()
 def mock_event_data(num_events=10):
     events = []
     for _ in range(num_events):
+        club = fake.company()
         content = fake.text(max_nb_chars=200)
-        time_post = fake.date_time_between(start_date="-1y", end_date="now", tzinfo=timezone.utc)
+        time_post = fake.date_time_between(start_date="-1y", end_date="now")
         likes = [random.randint(1, 100) for _ in range(random.randint(0, 10))]
-        event = Event(content=content, created_at=time_post, like=likes)
+        event = Event(club= club,content=content, created_at=time_post, like=likes)
         events.append(event)
     return events
-
-if __name__ == "__main__":
-    events_data = mock_event_data(num_events=5)
-    for event in events_data:
-        event.save()
-        print(event)
