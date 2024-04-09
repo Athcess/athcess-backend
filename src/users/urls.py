939f35d92a7d13614628c53fc612e2c94bb80ce0
Pataradanai Hansuksakul <116214675+Pataradanaimcky@users.noreply.github.com>
2024-04-10 04:50:58 +0700

    Update urls.py

--- src/users/urls.py
+++ src/users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     #mock data for dev only no prod.
+    path('mock/users/', mock_views.mock_custom_user, name='mock_custom_user'),
     path('mock/athlete/', mock_views.mock_athlete, name='mock_athlete'),
     path('mock/scout/', mock_views.mock_scout, name='mock_scout'),
     path('mock/admin/', mock_views.mock_admin, name='mock_admin'),
