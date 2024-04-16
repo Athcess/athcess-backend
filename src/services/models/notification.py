b88f7d794aaa98baa871a763ceff219e61c4fb73
Mondhuchchanun K <mondhuchchanun@gmail.com>
2024-04-17 02:19:53 +0700

    fix noti

--- src/services/models/notification.py
+++ src/services/models/notification.py
@@ -2,7 +2,6 @@ from django.db import models
 from users.models.custom_user import CustomUser
 
 
-
 class Notification(models.Model):
     notification_id = models.AutoField(primary_key=True)
     username = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
@@ -18,4 +17,4 @@ class Notification(models.Model):
         db_table = "notification"
 
     def __str__(self):
-        return f"Notification ID: {self.Noti_ID}"
+        return f"Notification ID: {self.notification_id}"
