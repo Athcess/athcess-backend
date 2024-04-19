e21042bf01c56ddeb40c1e16a5c40d9ad2b38a85
Pataradanai Hansuksakul <116214675+Pataradanaimcky@users.noreply.github.com>
2024-04-20 03:04:09 +0700

    Update analytic.py

--- src/services/apis/analytic.py
+++ src/services/apis/analytic.py
@@ -29,7 +29,32 @@ class BlobStorageViewSet(viewsets.ModelViewSet):
     def get_analytics(self, request, *args, **kwargs):
         username = request.query_params.get('player_name')
         physical_attribute_type = request.query_params.get('analytic_type')
+        else:
+            # Physical attribute does not exist, create a new one
+            if physical_attribute_type == 'sit_up':
+                count = count_situps(url)
+
+            elif physical_attribute_type == 'push_up':
+                count = count_push_ups(url)
+
+            elif physical_attribute_type == 'run':
+                speed = calculate_speed(url, height)
+                physical_attribute = {
+                    'create_at': timezone.now(),
+                    'username': username,
+                    'run': speed,
+                    'height': height,
+                }
+                serializer = PhysicalAttributeSerializer(data=physical_attribute)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         #get blobstorage with latest date
         blobstorage = BlobStorage.objects.filter(username=username, physical_attribute_type=physical_attribute_type).latest('created_at')
+        return Response({'message': 'Invalid analytic type. There are only 3 types of analytics: sit_up, push_up, run'},status=status.HTTP_400_BAD_REQUEST)
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
