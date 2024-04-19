78e6eaa2633bb8fb7cc977b2211e6b337bdd6668
Mondhuchchanun K <mondhuchchanun@gmail.com>
2024-04-19 16:57:05 +0700

    Update urls.py

--- src/services/urls.py
+++ src/services/urls.py
@@ -9,13 +9,16 @@ from .apis.notification import NotificationViewSet
 from .apis.organization import OrganizationViewSet
 from .apis.achievement import AchievementViewSets
 from .apis.experience import ExperienceViewSet
+from .apis.comment import CommentViewSet
+from .apis.like_comment import LikeCommentViewSet
 
 urlpatterns = [
-    path('search/', SearchViewSet.searched , name='searched'),
+    path('search/', SearchViewSet.searched, name='searched'),
     path('upload/', UploadFileViewSet.as_view({'post': 'create'}), name='upload'),
     path('upload/<int:pk>/', UploadFileViewSet.as_view({'put': 'update', 'delete': 'destroy'}), name='upload-detail'),
     path('post/', PostViewSet.as_view({'post': 'create', 'get': 'list'}), name='post'),
-    path('post/<int:pk>/', PostViewSet.as_view({'get': 'retrieve', }), name='post-detail'),
+    path('post/<int:pk>/', PostViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
+         name='post-detail'),
     path('like/<int:pk>/', LikeViewSet.as_view({'post': 'like'}), name='like'),
     path('users/<str:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update'}), name='user-detail'),
     path('calendar/get/', EventViewSet.as_view({'get': 'list'}), name='calendar-get'),
@@ -29,10 +32,18 @@ urlpatterns = [
     path('notification/create/', NotificationViewSet.as_view({'post': 'create'}), name='notification-create'),
     path('notification/delete/', NotificationViewSet.as_view({'delete': 'destroy'}), name='notification-delete'),
     path('organization/', OrganizationViewSet.as_view({'get': 'list'}), name='organization'),
-    path('organization/<str:org_name>/', OrganizationViewSet.as_view({'get': 'get_by_name'}), name='organization-detail'),
+    path('organization/<str:org_name>/', OrganizationViewSet.as_view({'get': 'get_by_name'}),
+         name='organization-detail'),
     path('achievement/', AchievementViewSets.as_view({'post': 'create', 'get': 'list'}), name='achievement'),
-    path('achievement/<str:pk>/', AchievementViewSets.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}), name='achievement-detail'),
+    path('achievement/<str:pk>/',
+         AchievementViewSets.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}),
+         name='achievement-detail'),
     path('achievement/mock/', AchievementViewSets.as_view({'post': 'mock_achievements'}), name='achievement-mock'),
     path('experience/', ExperienceViewSet.as_view({'post': 'create', 'get': 'list'}), name='experience'),
-    path('experience/<int:pk>/', ExperienceViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put':'update'}), name='experience-detail'),
+    path('experience/<int:pk>/', ExperienceViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}),
+         name='experience-detail'),
+    path('comment/', CommentViewSet.as_view({'post': 'create', 'get': 'list'}), name='comment-list'),
+    path('comment/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}),
+         name='comment-detail'),
+    path('like_comment/<int:pk>/', LikeCommentViewSet.as_view({'post': 'like'}), name='like-comment'),
 ]
