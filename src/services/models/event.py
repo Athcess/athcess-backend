467dac0387ee439692116da50747a5dbc86abf06
Mondhuchchanun K <mondhuchchanun@gmail.com>
2024-04-20 02:04:12 +0700

    Update event.py

--- src/services/models/event.py
+++ src/services/models/event.py
@@ -2,6 +2,7 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from users.models.custom_user import Organization
 
+
 class Event(models.Model):
     event_id = models.AutoField(primary_key=True)
     club = models.ForeignKey(Organization, on_delete=models.CASCADE)
@@ -14,9 +15,6 @@ class Event(models.Model):
 
     class Meta:
         db_table = "event"
-
-    def end_time(self):
-        return self.start_time + self.duration
 
     def __str__(self):
         return f"Event ID: {self.event_id}"
