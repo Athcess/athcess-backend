72f10abc77d1bc574d313bca001d3e06a643be8d
Mondhuchchanun K <mondhuchchanun@gmail.com>
2024-04-19 17:50:38 +0700

    Create repost.py

---
+++ src/services/apis/repost.py
@@ -0,0 +1,33 @@
+from users.models.custom_user import CustomUser, Athlete, Scout, Admin_organization, Organization
+from ..models.post import Post
+from rest_framework import viewsets, status, permissions, serializers
+from rest_framework.response import Response
+from django.utils import timezone
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['post_id', 'username', 'created_at', 'description', 'has_attachment', 'highlight']
+
+
+
+class RepostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        create_at = timezone.now()
+        serializer = PostSerializer(data={
+                                            'username': request.user.username,
+                                            'created_at': create_at,
+                                            'description': request.data['post_id'],
+                                            'highlight': request.data['highlight'],
+                                            'has_attachment': False
+                                          })
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
