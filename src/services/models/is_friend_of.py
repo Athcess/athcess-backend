b7f4a5a107b3fb6c4e5f705da5fb18c30704ed2d
Mondhuchchanun K <mondhuchchanun@gmail.com>
2024-04-20 13:55:45 +0700

    Update is_friend_of.py

--- src/services/models/is_friend_of.py
+++ src/services/models/is_friend_of.py
@@ -4,9 +4,9 @@ from users.models.custom_user import CustomUser
 
 class IsFriendOf(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_id')
-    friend_user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='friend_user_id')
-    status = models.CharField(max_length=50, choices=[('pending', 'pending'), ('accepted', 'accepted')])
+    username = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_id')
+    friend_username = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='friend_user_id')
+    status = models.CharField(max_length=50, choices=[('pending', 'pending'), ('accepted', 'accepted'), ('rejected', 'rejected')])
     since = models.DateTimeField(blank=True, null=True)
 
 
@@ -14,4 +14,4 @@ class IsFriendOf(models.Model):
         db_table = "is_friend_of"
     
     def __str__(self):
-        return str(self.friend_user_id)
+        return str(self.id)
