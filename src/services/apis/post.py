6af07cb572264dbc1830e758a8c5f99b8524487a
Mondhuchchanun K <mondhuchchanun@gmail.com>
2024-04-22 05:36:40 +0700

    Update post.py

--- src/services/apis/post.py
+++ src/services/apis/post.py
@@ -1,13 +1,16 @@
 from users.models.custom_user import CustomUser, Athlete, Scout, Admin_organization, Organization
 from ..models.post import Post
+from ..models.blob_storage import BlobStorage
 from rest_framework import viewsets, status, permissions, serializers
 from rest_framework.response import Response
 from django.utils import timezone
 
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['post_id', 'username', 'created_at', 'description', 'has_attachment', 'highlight', 'is_repost', 'likes']
+        fields = ['post_id', 'username', 'created_at', 'description', 'has_attachment', 'highlight', 'is_repost',
+                  'likes']
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -18,14 +21,14 @@ class PostViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         create_at = timezone.now()
         serializer = PostSerializer(data={
-                                            'username': request.user.username,
-                                            'created_at': create_at,
-                                            'description': request.data['description'],
-                                            'has_attachment': request.data['has_attachment'],
-                                            'highlight': request.data['highlight'],
-                                            'is_repost': False
+            'username': request.user.username,
+            'created_at': create_at,
+            'description': request.data['description'],
+            'has_attachment': request.data['has_attachment'],
+            'highlight': request.data['highlight'],
+            'is_repost': False
 
-                                          })
+        })
 
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -38,7 +41,15 @@ class PostViewSet(viewsets.ModelViewSet):
         for key, value in request.query_params.items():
             queryset = queryset.filter(**{key: value})
         serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+
+        posts = []
+        for i in serializer.data:
+            url = BlobStorage.objects.filter(username=i['username'], is_profile_picture=True).values_list('url',
+                                                                                                                 flat=True).first()
+            i['url'] = url
+            posts.append(i)
+
+        return Response(posts, status=status.HTTP_200_OK)
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
